7089b4a1dab32eb8958081d3a0139de3242a8721
Chinmoy M <chinmoy.mhptra@gmail.com>
2017-08-24 22:06:52 +0530

    updated the client/master code
    update to include the command send from master to slave, WIP

--- bt_client_1.py
+++ bt_client_1.py
@@ -1,33 +1,54 @@
 import bluetooth
+#import subprocess #required for chip only
 import ast
 
-port = 3
+#Global variables
+BTPortNo = 3
+TargetAddress = None
+command_list = ["['detectall']",\
+		"['initialize',0x10]",\
+		"['getinfo',0x10]",\
+		"['readfrom',0x10,0x30]",
+		"['writeto',0x10,0x30,0xff]",\
+		"quit"
+		]
 
-print("Searching for nearby devices...")
-NearbyDevices = bluetooth.discover_devices()
-print(NearbyDevices)
+#make the bluetooth device discoverable
+#subprocess.call(['sudo','hciconfig','hci0','piscan'])
 
-for device in NearbyDevices:
-        name = bluetooth.lookup_name(device)
-        addr = device
-        print(name,device)
-        if name=="chip":
-                print("connecting to chip")
-                target_address = addr
-                services = bluetooth.find_service(address=target_address)
-                #print(services)
-                break
-
-client = bluetooth.BluetoothSocket( bluetooth.RFCOMM )
-client.connect((target_address, port))
-
-
-print("connected")
-try:
-        while True:
-                RxData = ast.literal_eval(client.recv(1024))
-                print("Name = [%s]" % RxData[0])
-                #connect_to_i2c(RxData[0],RxData[1],RxData[2])
-                client.send("OK")
-except:
-        client.close()
+#main function/code
+if __name__ == "__main__":
+	#serach for devices
+	print("Searching for nearby devices...")
+	NearbyDevices = bluetooth.discover_devices()
+	print(NearbyDevices)
+	#connect to chip
+	for device in NearbyDevices:
+		name = bluetooth.lookup_name(device)
+		print (name,device)
+		if name=="chip":
+			print("Connecting to chip")
+			TargetAddress = device
+			#services = bluetooth.find_Service(address=target_address)
+			#print(services)
+			break
+	if TargetAddress == None:
+		print("No chip device avaialble")
+	else:
+		client = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+		client.connect((TargetAddress,BTPortNo))
+		print("connected to ",TargetAddress)
+		try:
+			for indx in range(0,5):
+				#command = raw_input()
+				command  = command_list[indx]
+				client.send(command)
+				if command == "quit":
+					print("Quit command")
+					client.close()
+				print(command)
+		except KeyboardInterrupt:
+			print("Shutting down...")
+			client.close()
+		except:
+			client.close()

750023599cc6b44bbf9871dc94da35369131cdd8
Chinmoy M <chinmoy.mhptra@gmail.com>
2017-08-24 13:03:08 +0530

    updated for both way communication

--- bt_server_1.py
+++ bt_server_1.py
@@ -1,35 +1,31 @@
-
 import bluetooth
 import subprocess
 
-
+#som eglobal variables
 subprocess.call(['sudo','hciconfig','hci0','piscan'])
-
+RxBuffSize = 1024
 port = 3
-server = bluetooth.BluetoothSocket( bluetooth.RFCOMM )
-server.bind(("",port))
-server.listen(1)
-#port = server.getsockname()[1]  
-
-#UUID = "00001800-0000-1000-8000-00805f9b34fb"
-UUID = "Generic Access Profile"
-
-#print("advertise my class")
-#bluetooth.advertise_service(server,"CHIP",service_id=UUID, \
-#service_classes = [(UUID,SERIAL_PORT_CLASS)],profiles=[SERIAL_PORT_PROFILE])
+command = "['DEVICE_NAME','READ/WRITE','VALUE']"
 
 
-print("Waiting for connection on RFCOMM port %d" % port)
-client, client_info = server.accept()
-print("Accepted connection from ", client_info)
+if __name__ == "__main__":
+        print("Starting Server...")
+        server = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+        server.bind(("",port))
+        server.listen(1)
+        print("Waiting connection on RFCOMM port %d" % port)
+        client,client_info = server.accept()
+        print("Connection to",client_info)
 
-try:
-    while True:
-        data = client.recv(1024)
-        if data == "quit": 
-		break
-        print("received [%s]" % data)
-except: 
-	print("disconnected")
-	client.close()
-	server.close()
+        try:
+                while True:
+                        #command = raw_input()
+                        client.send(command)
+                        RxData = client.recv(RxBuffSize)
+                        if RxData == "quit":
+                                print("Connection ended from client")
+                                break
+                        print("Recieved [%s]"%RxData)
+        except:
+                client.close()
+                server.close()

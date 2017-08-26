edc941a334d8ea70b5e4aca3fc93ce2641d6cfbb
Chinmoy M <chinmoy.mhptra@gmail.com>
2017-08-26 11:34:11 +0530

    Update I2C_wrapper.py

--- I2C_wrapper.py
+++ I2C_wrapper.py
@@ -3,26 +3,28 @@ import sensor_trial
 import time
 import sys
 
+def get_addr(device_name):	
+	return 0x77
 
 def detectall():
-	device_addr = 0x77
-	return device_addr
+	device = 'BMP085'
+	return device
 
-def getinfo(addr):
-	if addr is 0x77:
-		sensor1 = sensor_trial.BMP085()
-		return sensor1.info
+def getinfo(device_name):
+	sensor = device_mame.info()
+	#sensor = sensor_trial.BMP085()
+	return sensor
 
-def read_from(device_addr,register):
-	if device_addr is 0x77:
-		device = I2C.get_i2c_device(device_addr)
-		ans = device.readU8(register)
-		return ans
+def read_from(device_name,register):
+	device_addr = get_addr(device_name)
+	device = I2C.get_i2c_device(device_addr)
+	ans = device.readU8(register)
+	return ans
 
-def write_to(device_addr,register,value):
-	if device_addr is 0x77:
-		device = I2C.get_i2c_device(device_addr)
-		device.write8(register,value)
+def write_to(device_name,register,value):
+	device_addr = get_address(device_name)
+	device = I2C.get_i2c_device(device_addr)
+	device.write8(register,value)
 
 
 def get_temperature(device_addr = 0x77):

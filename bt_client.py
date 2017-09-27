ce6021a943b140741c09ae2ce9a9c038af18baa1
Chinmoy M <chinmoy.mhptra@gmail.com>
2017-09-27 21:22:43 +0530

    MAC, address, error, dictionary - WIP

--- bt_client.py
+++ bt_client.py
@@ -1,117 +1,154 @@
+############################################################################################################
+		## LIBRARIES
 import bluetooth
 import subprocess #required for chip only
 import ast
-
 from time import sleep
 
-#Global variables
-BTPortNo = 3
-TargetAddress = None
-#dummy command list to test
-command_list = ['[\"detectall\"]',\
-		'[\"initialize\",\"BMP085\"]',\
-		'[\"getinfo\",\"BMP085\"]',\
-		'[\"readfrom\",\"BMP085\",0xF4]',\
-		'[\"writeto\",\"BMP085\",0xF4,0x34]',\
-		'[\"readfrom\",\"BMP085\",0xF4]',\
-		'[\"Get_Temp\"]',\
-		'[\"Get_Pressure\"]',\
-		"quit"\
-		]
 
+
+###########################################################################################################
+		## Global variables
+BTPortNo = 3
+TargetMACAddress = '68:17:29:45:5F:32'
+AddressList = ''
+client = ''
+
+
+###########################################################################################################
+		## Functions and utilities
 
 #make the bluetooth device discoverable
 subprocess.call(['sudo','hciconfig','hci0','piscan'])
 
-#main function/code
+# detectall command
+# sysntax : detectall
+def detectall():
+	client.send("detectall")
+	AddressList = client.recv(1024)
+	AddressList = ast.literal_eval(AddressList)
+	print(AddressList)	
+
+# initialize command
+# syntax : initialize [address]
+def initialize():
+	print("Enter address of device to initialize:")
+	DeviceAddress = raw_input()
+	command = '["initialize",'+DeviceAddress+']'##################################################################################
+	client.send(command)
+	Ack = client.recv(1024)
+	print Ack
+
+# getinfo command
+# syntax : getinfo [device address]
+def getinfo():
+	print("Enter device address for device info:")
+	DeviceAddress = raw_input()
+	command = '["getinfo",'+DeviceAddress+']'############################################
+	client.send(command)
+	DeviceInfo = client.recv(1024)
+	DeviceInfo = ast.literal_eval(DeviceInfo)
+	print(DeviceInfo)
+
+# readform command
+# syntax : readfrom [device address] [register address]
+def readfrom():
+	print("Enter device address:")
+	DeviceAddress = raw_input()
+	print("Enter register address:")
+	RegisterAddress = raw_input()
+	command = '["readfrom",'+DeviceAddress+','+RegisterAddress+']'#########################################
+	client.send(command)
+	RegisterValue = client.recv(1024)
+	print(RegisterValue)
+
+# writeto command
+# syntax: writeto [device address][register address]
+def writeto():
+	print("Enter device address:")
+	DeviceAddress = raw_input()
+	print("Enter register address:")
+	RegisterAddress = raw_input()
+	command = '["writeto",'+DeviceAddress+','+RegisterAddress+']'##########################################
+	client.send(command)
+	WriteAck = client.recv(1024)
+	print(WriteAck)
+
+
+##########################################################################################################
+		## command dictionary to lookup
+command_dictionary = {   "detectall" 	: detectall, \
+			"initialize" 	: initialize, \
+			"getinfo"	: getinfo, \
+			"readfrom"	: readfrom, \
+			"writeto"	: writeto\
+		    }
+
+
+
+###########################################################################################################
+		## main function/code
+
 if __name__ == "__main__":
-	#serach for devices
-	print("Searching for nearby devices...")
-	NearbyDevices = bluetooth.discover_devices()
-	print(NearbyDevices)
-	#connect to chip
-	for device in NearbyDevices:
-		name = bluetooth.lookup_name(device)
-		print (name,device)
-		if name=="chip":
-			print("Connecting to chip")
-			TargetAddress = device
-			services = bluetooth.find_service(address=TargetAddress)
-			print(services)
-			break
-	if TargetAddress == None:
-		print("No chip device avaialble")
-	else:
-		client = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
-		client.connect((TargetAddress,BTPortNo))
-		print("connected to ",TargetAddress)
-		print ""
-		try:
-			for command in command_list	:
-				#command = raw_input()
+	
+	try:
+		while(1):
+			# search for BT devices
+			print("Searching for nearby devices...")
+			try:
+				NearbyDevices = bluetooth.discover_devices()
+			except:
+				print("unable to discover device, check your bluetooth settings")
+			print(NearbyDevices)
+	
+			# discover and connect to chip
+			for Address in NearbyDevices:
+				# discover chip based on MAC address
+				try:
+					name = bluetooth.lookup_name(Address)
+				except:
+					print("unable to fetch name")
+				print (name,Address,TargetMACAddress)
+				if Address == TargetMACAddress:
+					print("Services available with chip server",TargetMACAddress)
+					services = bluetooth.find_service(address=TargetMACAddress)
+					print(services)
+					print('')
+					print("Do you wish to connect[n for no]")
+					con = raw_input()
+					if con == "n":
+						continue
 
-				if command == "quit":
-					print("Quit command")
-					client.close()
-				client.send(command)
+					# connect to a chip server 
+					print("connecting to chip server",TargetMACAddress)
+					client = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+					client.connect((TargetMACAddress,BTPortNo))
+					print("connected to ",TargetMACAddress)
+					connected = 1
 
-				print command
-
-
-				if command == command_list[0]:				#detectall
-					add_list = client.recv(1024)
-					add_list = ast.literal_eval(add_list)
-					for add in add_list:
-						print add
-					print ""
-
-
-				elif command == command_list[1]:			#initialize
-					Ack_packet = client.recv(1024)
-					print Ack_packet
-					print ""
-
-				elif command == command_list[2]:			#getinfo
-					Dev_info = client.recv(1024)
-					print  "Device name is " + ast.literal_eval(Dev_info)[0]
-					Dev_info = ast.literal_eval(ast.literal_eval(Dev_info)[1])
-					print "Available Registers and commands are:" 
-					print Dev_info[0]
-					print Dev_info[1]
-					print Dev_info[2]
-					print Dev_info[3]
-					print Dev_info[4]
-					print ""
-
-				elif command == command_list[3]:			#readfrom
-					Register = client.recv(1024)
-					#print Register
-					print "Value of Register " + ast.literal_eval(Register)[1] + " Of Device at add " + ast.literal_eval(Register)[0] +" is " + ast.literal_eval(Register)[2]
-					print ""
-
-				elif command == command_list[4]:			#writeto
-					print client.recv(1024)
-					print ""
-
-				elif command == command_list[5]:			#readfrom
-					Register = client.recv(1024)
-					print "Value of Register " + ast.literal_eval(Register)[1] + " Of Device at add " + ast.literal_eval(Register)[0] +" is " + ast.literal_eval(Register)[2]
-					print ""	
-
-				elif command == command_list[6]:			#get temperature
-					print "Temperature is " + ast.literal_eval(client.recv(1024))[0] + " degree celcius"
-					print ""
-				
-				elif command == command_list[7]:			#get pressure
-					print "Pressure is " + ast.literal_eval(client.recv(1024))[0] + "Pa"
-					print ""
-
-
-
-
-				sleep(1)
-		except KeyboardInterrupt:
-			print("Shutting down...")
-			client.close()
-		except:
-			client.close()
+					# send commands to server
+					while(connected):
+						print("Enter command to be send to server")
+						print("Supported commands: [detectall][initialize][quit]")
+						command = raw_input();
+						if command == "quit": # quit command to exit only client
+							print("closing client")
+							client.close()
+							connected = 0
+							break
+						elif command not in command_list: # inavalid commands
+							print("command not in command list"+"\n")
+						else: # valid commands
+							try:
+								command_dictionary[command]()
+								sleep(1)
+							except KeyboardInterrupt:
+								print("Closing client")
+								client.close()
+							except:
+								print("closing client")
+								client.close()
+	except KeyboardInterrupt:
+		print("exit")
+	except:
+		print("exit")

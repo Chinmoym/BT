91240941fd323732d8506f06d193719f028771d3
Nihar Desai <nihar.desai1@gmail.com>
2017-09-27 22:54:13 +0530

    changed to command by addr, add scan_i2c_bus(),

--- sensor_top.py
+++ sensor_top.py
@@ -1,70 +1,74 @@
 import Adafruit_GPIO.I2C as I2C
 import sensor_trial
+import smbus_scan
 import time
 import sys
 
-def get_addr(device_name):
-	return 0x77
+
+def get_name(dev_addr):
+        if dev_addr == 0x77:
+                return 'BMP085'
 
 def detectall():
-	device = 'BMP085'
-	return device
-
-def getinfo(device_name):
-	sensor = getattr(sensor_trial,device_name)		#This will return device information
-	return sensor.info
-
-def initialize(device_name):
-	sensor = getattr(sensor_trial,device_name)		#This will initialize the device by loading default calibration
-	return sensor
-
-def read_from(device_name,register):
-	device_addr = get_addr(device_name)
-	device = I2C.get_i2c_device(device_addr)
-	ans = device.readU8(register)
-	return ans
-
-def write_to(device_name,register,value):
-	device_addr = get_addr(device_name)
-	device = I2C.get_i2c_device(device_addr)
-	device.write8(register,value)
+        return smbus_scan.scan_i2c_bus()
 
 
-def get_temperature(device_name= 'BMP085'):
-	device_addr = get_addr(device_name)
-	if device_addr is 0x77:
-		sensor1 = sensor_trial.BMP085()
-		a = sensor1.read_temperature()
-		return a
+def getinfo(device_addr):
+        device_name = get_name(device_addr)
+        sensor = getattr(sensor_trial,device_name)              #This will return device information
+        return sensor.info
 
-def get_pressure(device_name='BMP085'):
-	device_addr = get_addr(device_name)
-	if device_addr is 0x77:
-		sensor1 = sensor_trial.BMP085()
-		a = sensor1.read_pressure()
-		return a
+def initialize(device_addr):
+        device_name = get_name(device_addr)
+        sensor = getattr(sensor_trial,device_name)              #This will initialize the device by loading default calibration
+        return sensor
+
+def read_from(device_addr,register):
+        device = I2C.get_i2c_device(device_addr)
+        ans = device.readU8(register)
+        return ans
+
+def write_to(device_addr,register,value):
+        device = I2C.get_i2c_device(device_addr)
+        device.write8(register,value)
+
+
+def get_temperature(device_addr):
+        device_name = get_name(device_addr)
+        sensor1 = getattr(sensor_trial,device_name)
+        temperature = sensor1.read_temperature()
+        return temperature
+
+def get_pressure(device_addr):
+        device_name = get_name(device_addr)
+        sensor1 = getattr(sensor_trial,device_name)
+        pressure = sensor1.read_pressure()
+        return pressure
 
 
 ##This is for debugging
+print(detectall())
 
-"""print(getinfo('BMP085'))
+"""print(getinfo(0x77))
 time.sleep(1)
-print(initialize('BMP085'))
+print(initialize(0x77))
 time.sleep(1)
 
-print(read_from('BMP085',0xF4))
+print(read_from(0x77,0xF4))
 time.sleep(0.5)
 
-write_to('BMP085',0xF4,0x2E)
+write_to(0x77,0xF4,0x2E)
 time.sleep(0.5)
 
-for i in range(1,10):
-	print('------------------')
-	write_to('BMP085',0xF4,0x2E)
-	print(read_from('BMP085',0xF7))
-	time.sleep(0.5)
-	write_to('BMP085',0xF4,0x34)
-        print(read_from('BMP085',0xF7))
+for i in range(1,5):
+        print('------------------')
+        write_to(0x77,0xF4,0x2E)
+        print(read_from(0x77,0xF6))
+        time.sleep(0.5)
+        write_to(0x77,0xF4,0x34)
+        print(read_from(0x77,0xF6))
+        time.sleep(0.5)"""
+
         time.sleep(0.5)"""
 		
 

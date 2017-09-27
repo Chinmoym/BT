4743d5bdf6f88ac9e1211fd55fbff8becfc40750
Chinmoy M <chinmoy.mhptra@gmail.com>
2017-09-27 22:42:28 +0530

    infinite loop, address - WIP

--- bt_server.py
+++ bt_server.py
@@ -1,85 +1,97 @@
-#inlcude library dependencies
+#############################################################################################################
+		## library dependencies
 import bluetooth
 import subprocess
 import ast
-import sensor_top
+#import sensor_top
 
-#Global variables
-RxBuffSize = 4096
+
+############################################################################################################
+		## Global variables
+
+RxBuffSize = 1024
 BTPortNo = 3
+ServerOn = 1
+client = ''
 
-#make the bluetooth device discoverable
-subprocess.call(['sudo','hciconfig','hci0','piscan'])
+############################################################################################################
+		## functions and utilities
+
+#make the bluetooth device discoverable (onlt in chip)
+#subprocess.call(['sudo','hciconfig','hci0','piscan'])
+
+def detectall(x): # detectall command
+	print(x)
+	data = sensor_top.detectall()
+	#TxData = '[\"'+data+'\"]'
+	client.send(Data)
+
+def getinfo(x): # getinfo command
+	print (x)
+	data = sensor_top.getinfo(x[1])
+	client.send(data)
+
+def readfrom(x):
+	print(x)
+	data = sensor_top.readfrom(x[1],x[2])
+	data = format(data, '#04X')
+	client.send(data)
+
+def writeto(x):
+	print(x)
+	sensor_top.write_to(x[1],x[2],x[3])
+	client.send("write successfull")
 
 
-#main function/ code
+###########################################################################################################
+		## command dictionary to lookup
+command_dictionary = {	"detectall"	: detectall, \
+			#"initialize"	: initialize, \
+			"getinfo"	: getinfo, \
+			"readfrom"	: readfrom, \
+			"writeto"	: writeto\
+		     }
+
+
+###########################################################################################################
+		## main function/ code
+
 if __name__ == "__main__":
-	print("Starting server...")
-	#create bluetooth socket and acept connection from master
-	server = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
-	server.bind(("",BTPortNo))
-	server.listen(1)
-	print("Waiting for connection from master [RFCOMM port=%d]" % BTPortNo)
-	client,client_info = server.accept()
-	print("Connected to",client_info)
-	#send/recieve to/from master
+	
 	try:
-		while True:
-			RxData = client.recv(RxBuffSize)
-			print(RxData)
-			#end the connection from master
-			if RxData=="quit":
-				print("Master disconnected")
-				client.close()
-				#wait for another connection
-				print("Waiting for connection from master [RFCOMM port=%d]" % BTPortNo)
-				client,client_info = server.accept()
-				print("Connected to",client_info)
-			
-			Rx = ast.literal_eval(RxData)
+		print("Starting server...")
+		
+		#create bluetooth socket
+		try:
+			server = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+			server.bind(("",BTPortNo))
+			server.listen(1)
+		except:
+			print("unable to create/bind scocket")
+	
+		while(ServerOn):
+			connected = 0
+			while(not connected):
+				# accept connection from client
+				print("Waiting for connection from client on [RFCOMM port=%d]" % BTPortNo)
+				try:
+					client,client_info = server.accept()
+					print("Connected to",client_info)
+					connected = 1
+				except:
+					pass
 
-			if Rx[0]=="detectall":
-				data = sensor_top.detectall()
-				data_send = '[\"' + data +'\"]'
-				client.send(data_send)
+			#send/recieve to/from client
+			try:
+				while connected:
+					RxData = client.recv(RxBuffSize)
+					RxData = ast.literal_eval(RxData)
+					print(RxData)
 
-			elif Rx[0]=="getinfo":
-				print(Rx[1])
-				info = sensor_top.getinfo(Rx[1])
-				client.send(info)
+					command_dictionary[RxData[0]](RxData)
 
-			elif Rx[0]=="readfrom":
-				data = sensor_top.read_from(Rx[1],Rx[2])
-				data = format(data, '#04X')
-				ret1 = Rx[1]
-				ret2 = str(format(Rx[2], '#04X'))
-				data_send = '[\"' + ret1 + '\",\"' + ret2 +'\", \"' + str(data) + '\"]'
-				client.send(data_send)
-
-			elif Rx[0]=="writeto":
-				sensor_top.write_to(Rx[1],Rx[2],Rx[3])
-				client.send("Write OK")
-
-			elif Rx[0]=="initialize":
-				sensor_top.initialize(Rx[1])
-				data_send =  Rx[1] + ' is initialized' 
-				client.send(data_send)
-
-			elif Rx[0]=="Get_Temp":
-				data = sensor_top.get_temperature()
-				data_send = '[\"' + str(data) + '\"]'
-				client.send(data_send)
-
-			elif Rx[0]=="Get_Pressure":
-                                data = sensor_top.get_pressure()
-                                data_send = '[\"' + str(data) + '\"]'
-				print(data_send)           
-                                client.send(data_send)
-				
-			else:
-				client.send("Wrong Input")
-				print("Wrong input received")
-				
+			except:
+				pass
 
 	#handle exceptions/ctrl + c
 	except KeyboardInterrupt:
@@ -89,6 +101,3 @@ if __name__ == "__main__":
 	except:
 		client.close()
 		server.close()		
-		
-	
-

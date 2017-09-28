aad15ea361268914c6f13e7b1e8e6aa17156c522
Chinmoy M <chinmoy.mhptra@gmail.com>
2017-09-28 18:24:33 +0530

    update MAC, address, interactive
    update the code to connect using MAC address
    send interactive response to client
    used dictionary method, added some error handling steps

--- bt_server.py
+++ bt_server.py
@@ -3,44 +3,66 @@
 import bluetooth
 import subprocess
 import ast
-#import sensor_top
+import sensor_top
 
 
 ############################################################################################################
 		## Global variables
 
-RxBuffSize = 1024
+RxBuffSize = 4096
 BTPortNo = 3
 ServerOn = 1
 client = ''
+connected = 0
 
 ############################################################################################################
 		## functions and utilities
 
 #make the bluetooth device discoverable (onlt in chip)
-#subprocess.call(['sudo','hciconfig','hci0','piscan'])
+subprocess.call(['sudo','hciconfig','hci0','piscan'])
 
 def detectall(x): # detectall command
-	print(x)
 	data = sensor_top.detectall()
-	#TxData = '[\"'+data+'\"]'
-	client.send(Data)
+	TxData = '[\"'+data+'\"]'
+	try:
+		client.send(TxData)
+	except:
+		print("error sending reply")
+		pass
 
 def getinfo(x): # getinfo command
-	print (x)
 	data = sensor_top.getinfo(x[1])
-	client.send(data)
+	TxData = '[\"'+data+'\"]'
+	try:
+		client.send(TxData)
+	except:
+		print("error sending reply")
+		pass
 
 def readfrom(x):
-	print(x)
+	print("readfrom::",x,x[1],x[2])
 	data = sensor_top.readfrom(x[1],x[2])
-	data = format(data, '#04X')
-	client.send(data)
+	print(data)
+	TxData = '[\"'+str(data)+'\"]'
+	print(TxData)
+	try:
+		client.send(TxData)
+	except:
+		print("error sending reply")
+		pass
 
 def writeto(x):
-	print(x)
-	sensor_top.write_to(x[1],x[2],x[3])
-	client.send("write successfull")
+	sensor_top.writeto(x[1],x[2],x[3])
+	TxData = '[\"write successfull\"]'
+	try:
+		client.send(TxData)
+	except:
+		print("error sending reply")
+		pass
+
+def quit__():
+	client.close()
+	
 
 
 ###########################################################################################################
@@ -49,7 +71,8 @@ command_dictionary = {	"detectall"	: detectall, \
 			#"initialize"	: initialize, \
 			"getinfo"	: getinfo, \
 			"readfrom"	: readfrom, \
-			"writeto"	: writeto\
+			"writeto"	: writeto, \
+			"quit"		: quit__
 		     }
 
 
@@ -57,47 +80,37 @@ command_dictionary = {	"detectall"	: detectall, \
 		## main function/ code
 
 if __name__ == "__main__":
-	
-	try:
-		print("Starting server...")
-		
-		#create bluetooth socket
-		try:
-			server = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
-			server.bind(("",BTPortNo))
-			server.listen(1)
-		except:
-			print("unable to create/bind scocket")
-	
-		while(ServerOn):
-			connected = 0
-			while(not connected):
-				# accept connection from client
-				print("Waiting for connection from client on [RFCOMM port=%d]" % BTPortNo)
-				try:
-					client,client_info = server.accept()
-					print("Connected to",client_info)
-					connected = 1
-				except:
-					pass
 
-			#send/recieve to/from client
+	print("Starting server...")
+	#create bluetooth socket and acept connection from master
+	server = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+	server.bind(("",BTPortNo))
+	server.listen(1)
+	try:
+		while(1):
+			print("Waiting for connection from master [RFCOMM port=%d]" % BTPortNo)
+			client,client_info = server.accept()
+			print("Connected to",client_info)
+			#send/recieve to/from master
 			try:
-				while connected:
+				while True:
 					RxData = client.recv(RxBuffSize)
 					RxData = ast.literal_eval(RxData)
-					print(RxData)
-
+					print("Recieved command:",RxData)	
 					command_dictionary[RxData[0]](RxData)
 
+			#handle exceptions/ctrl + c
+			except KeyboardInterrupt:
+				print("Shutting down...")
+				client.close()
+				server.close()
 			except:
-				pass
-
-	#handle exceptions/ctrl + c
+				client.close()
 	except KeyboardInterrupt:
-		print("Shutting down...")
-		client.close()
-		server.close()
-	except:
+		print("Shuttig down...")
 		client.close()
 		server.close()		
+	except:
+		print("exit")
+		client.close()
+		server.close()

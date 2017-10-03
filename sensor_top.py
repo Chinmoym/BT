ebc48237a01dae975b3f6a889c408dcc4a7636c5
Nihar Desai <nihar.desai1@gmail.com>
2017-10-03 11:41:52 +0530

    Error handlhing

--- sensor_top.py
+++ sensor_top.py
@@ -1,74 +1,82 @@
-#Implementation of functions used in bt_server.py
-
 import Adafruit_GPIO.I2C as I2C
 import sensor_trial
 import smbus_scan
 import time
 import sys
 
+device_dictionary =  { 0x77 : 'BMP085'
+}
 
 def get_name(dev_addr):
-        if dev_addr == 0x77:
-                return 'BMP085'
+	return device_dictionary[dev_addr]
 
 def detectall():
-        return smbus_scan.scan_i2c_bus()
-
+	return smbus_scan.scan_i2c_bus()
+	
 
 def getinfo(device_addr):
-        device_name = get_name(device_addr)
-        sensor = getattr(sensor_trial,device_name)              #This will return device information
-        return sensor.info
+	if device_addr in device_dictionary:
+		device_name = get_name(device_addr)
+		sensor = getattr(sensor_trial,device_name)		#This will return device information
+		return sensor.info
+	else:
+		return "Device Not Found"
 
 def initialize(device_addr):
-        device_name = get_name(device_addr)
-        sensor = getattr(sensor_trial,device_name)              #This will initialize the device by loading default calibration
-        return sensor
+	device_name = get_name(device_addr)
+	sensor = getattr(sensor_trial,device_name)		#This will initialize the device by loading default calibration
+	return sensor
 
-def read_from(device_addr,register):
-        device = I2C.get_i2c_device(device_addr)
-        ans = device.readU8(register)
-        return ans
+def readfrom(device_addr,register):
+	try:
+		device = I2C.get_i2c_device(device_addr)
+		ans = device.readU8(register)
+		return ans
+	except IOError:
+		return "No Device Found"
 
-def write_to(device_addr,register,value):
-        device = I2C.get_i2c_device(device_addr)
-        device.write8(register,value)
-
+def writeto(device_addr,register,value):
+	try:
+		device = I2C.get_i2c_device(device_addr)
+		device.write8(register,value)
+	except IOError:
+		return "No Device found"
 
 def get_temperature(device_addr):
-        device_name = get_name(device_addr)
-        sensor1 = getattr(sensor_trial,device_name)
-        temperature = sensor1.read_temperature()
-        return temperature
+	device_name = get_name(device_addr)
+	sensor1 = getattr(sensor_trial,device_name)
+	temperature = sensor1.read_temperature()
+	return temperature
 
 def get_pressure(device_addr):
-        device_name = get_name(device_addr)
-        sensor1 = getattr(sensor_trial,device_name)
-        pressure = sensor1.read_pressure()
-        return pressure
+	device_name = get_name(device_addr)
+	sensor1 = getattr(sensor_trial,device_name)
+	pressure = sensor1.read_pressure()
+	return pressure
 
 
 ##This is for debugging
 """print(detectall())
+print(get_name(119))
 
-print(getinfo(0x77))
+print(getinfo(119))
 time.sleep(1)
 print(initialize(0x77))
 time.sleep(1)
 
-print(read_from(0x77,0xF4))
+print(readfrom(0x77,0xF4))
 time.sleep(0.5)
 
-write_to(0x77,0xF4,0x2E)
+writeto(0x77,0xF4,0x2E)
 time.sleep(0.5)
 
 for i in range(1,5):
-        print('------------------')
-        write_to(0x77,0xF4,0x2E)
-        print(read_from(0x77,0xF6))
-        time.sleep(0.5)
-        write_to(0x77,0xF4,0x34)
-        print(read_from(0x77,0xF6))
+	print('------------------')
+	writeto(0x77,0xF4,0x2E)
+	print(readfrom(0x77,0xF6))
+	time.sleep(0.5)
+	writeto(0x77,0xF4,0x34)
+        print(readfrom(0x77,0xF6))
         time.sleep(0.5)"""
-
+		
 

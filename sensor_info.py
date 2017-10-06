bd25f927389bcdec8369acd3c051c31ddd3efacd
Nihar Desai <nihar.desai1@gmail.com>
2017-10-06 19:25:29 +0530

    Dictionary of sensors

---
+++ sensor_info.py
@@ -0,0 +1,4 @@
+
+device_dictionary =  { 0x77 : "['BMP085','[[\"0xF4\",\"CONTROL_REG\"],[\"0x2E\",\"READ_TEMPCMD\"],[\"0x34\",\"READ_PRESSURECMD\"],[\"0xF6\",\"UT_VALUE\"],[\"0xAA:0XBF\",\"E2_PROM\"]]']"
+
+}

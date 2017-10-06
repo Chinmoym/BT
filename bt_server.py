9fbc7f8e4720d237d28f9f228bef2bb7afc113c9
Nihar Desai <nihar.desai1@gmail.com>
2017-10-06 19:17:04 +0530

    support multi-client, advertse_service

--- bt_server.py
+++ bt_server.py
@@ -4,7 +4,7 @@ import bluetooth
 import subprocess
 import ast
 import sensor_top
-
+import thread
 
 ############################################################################################################
 		## Global variables
@@ -21,16 +21,19 @@ connected = 0
 #make the bluetooth device discoverable (onlt in chip)
 subprocess.call(['sudo','hciconfig','hci0','piscan'])
 
-def detectall(x): # detectall command
+def detectall(x,client): # detectall command
+	#print(x,"ok1")
 	data = sensor_top.detectall()
-	TxData = '[\"'+data+'\"]'
+	#print(data)
+	TxData = '[\"'+str(data)+'\"]'
+	#print(TxData)
 	try:
 		client.send(TxData)
 	except:
 		print("error sending reply")
 		pass
 
-def getinfo(x): # getinfo command
+def getinfo(x,client): # getinfo command
 	data = sensor_top.getinfo(x[1])
 	TxData = '[\"'+data+'\"]'
 	try:
@@ -39,7 +42,7 @@ def getinfo(x): # getinfo command
 		print("error sending reply")
 		pass
 
-def readfrom(x):
+def readfrom(x,client):
 	print("readfrom::",x,x[1],x[2])
 	data = sensor_top.readfrom(x[1],x[2])
 	print(data)
@@ -51,7 +54,7 @@ def readfrom(x):
 		print("error sending reply")
 		pass
 
-def writeto(x):
+def writeto(x,client):
 	sensor_top.writeto(x[1],x[2],x[3])
 	TxData = '[\"write successfull\"]'
 	try:
@@ -73,7 +76,21 @@ command_dictionary = {	"detectall"	: detectall, \
 			"readfrom"	: readfrom, \
 			"writeto"	: writeto, \
 			"quit"		: quit__
-		     }
+			}
+
+###########################################################################################################
+
+#create new thread for each client
+def new_client(threadname,delay,client,client_info):
+        try:
+                print(client,client_info)
+                while True:
+                        RxData = client.recv(1024)
+                        RxData = ast.literal_eval(RxData)
+                        print(client_info,"Recieved command:",RxData)
+                        command_dictionary[RxData[0]](RxData,client)
+        except:
+                pass
 
 
 ###########################################################################################################
@@ -86,26 +103,30 @@ if __name__ == "__main__":
 	server = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
 	server.bind(("",BTPortNo))
 	server.listen(1)
-	try:
-		while(1):
-			print("Waiting for connection from master [RFCOMM port=%d]" % BTPortNo)
-			client,client_info = server.accept()
-			print("Connected to",client_info)
-			#send/recieve to/from master
-			try:
-				while True:
-					RxData = client.recv(RxBuffSize)
-					RxData = ast.literal_eval(RxData)
-					print("Recieved command:",RxData)	
-					command_dictionary[RxData[0]](RxData)
 
+	strin = "Chip device"
+	myinfo = sensor_top.detectall()
+	
+	bluetooth.advertise_service(server,strin,service_classes = [bluetooth.SERIAL_PORT_CLASS],\
+					profiles=[bluetooth.SERIAL_PORT_PROFILE]\
+					, description = str(myinfo))
+	try:
+		print("Waiting for connection from master [RFCOMM port=%d]" % BTPortNo)
+		while(1):
+			try:
+				client,client_info = server.accept()
+				print("Connected to",client_info)
+				thread_name = '_'+client_info[0]
+				thread.start_new_thread(new_client,(thread_name,0,client,client_info))
+							
 			#handle exceptions/ctrl + c
 			except KeyboardInterrupt:
 				print("Shutting down...")
 				client.close()
 				server.close()
 			except:
-				client.close()
+				pass
+				#client.close()
 	except KeyboardInterrupt:
 		print("Shuttig down...")
 		client.close()

553af7125cba9af1fe7e27e275b1e0b65070267c
Nihar Desai <nihar.desai1@gmail.com>
2017-10-12 08:44:26 +0530

    add new sensors

--- sensor_info.py
+++ sensor_info.py
@@ -1,4 +1,5 @@
 
-device_dictionary =  { 0x77 : "['BMP085','[[\"0xF4\",\"CONTROL_REG\"],[\"0x2E\",\"READ_TEMPCMD\"],[\"0x34\",\"READ_PRESSURECMD\"],[\"0xF6\",\"UT_VALUE\"],[\"0xAA:0XBF\",\"E2_PROM\"]]']"
-
+device_dictionary =  { 0x77 : "['BMP085','[[\"0xF4(W)\",\"CONTROL_REG\"],[\"0x2E(CMD)\",\"READ_TEMPCMD\"],[\"0x34(CM$
+                       0x53 : "['ADXL345','[[\"0x1E:0x20(RW)\",\"[X][Y][Z]OFFSET\"],[\"0x2D(RW)\",\"PWR_CTL\"],[\"0x$
+                       0x68 : "['RTC_DS3231']"
 }

27eef9cba1562a26966754bca3a5dd69626b53f9
Nihar Desai <nihar.desai1@gmail.com>
2017-12-04 12:26:27 +0530

    Comments added

--- performance/SPI/spi_top.py
+++ performance/SPI/spi_top.py
@@ -1,19 +1,19 @@
-import Adafruit_GPIO.SPI as SPI
-import CHIP_IO.OverlayManager as OM
-import CHIP_IO.GPIO as gpio
+import Adafruit_GPIO.SPI as SPI				#SPI library
+import CHIP_IO.OverlayManager as OM			# To load/unload SPI
+import CHIP_IO.GPIO as gpio				# To configure GPIO
 import time
 
-OM.load("SPI2")
+OM.load("SPI2")			#load SPI2 
 
 
-ADC_CS = "U14_32"
+ADC_CS = "U14_32"		#GPIO Pin to be used as Chip select for SPI (here , device is 12-bit ADC MCP3202)
 
 gpio.setup(ADC_CS,gpio.OUT)		#CS for ADC
-gpio.output(ADC_CS,gpio.HIGH)
+gpio.output(ADC_CS,gpio.HIGH)		#Set as output
 
 """configure SPI"""
 def SPI_configure():
-	spi = SPI.SpiDev(2, 0, 500000)
+	spi = SPI.SpiDev(2, 0, 500000)		# create a class for SPI bus2,device 0, 500kHz clock.
 	spi.set_bit_order(SPI.MSBFIRST)
 	spi.set_clock_hz(500000)
 	spi.set_mode(0)
@@ -21,15 +21,16 @@ def SPI_configure():
 
 """ Transfer from SPI, for full Duplex"""
 def SPI_transfer(spi,send_data):
-	data = [0x00,0x00]
+	data = [0x00,0x00]				# received data tobe stored in 'data'
 	try:
-		gpio.output(ADC_CS,gpio.LOW)
-		data = spi.transfer(send_data)
-		gpio.output(ADC_CS,gpio.HIGH)
+		gpio.output(ADC_CS,gpio.LOW)		# Choose ADC to transfer
+		data = spi.transfer(send_data)		# get 12-bit ADC data
+		gpio.output(ADC_CS,gpio.HIGH)		
 	except:
 		print('SPI Error')
 	return data
 
+"""Write in SPI device, this will not work for ADC which we are using""" 
 def SPI_write(spi,send_data):   
         try:
                 gpio.output(ADC_CS,gpio.LOW)
@@ -38,6 +39,7 @@ def SPI_write(spi,send_data):
         except:
                 print("SPI write Error")
 
+"""Get data from SPI device, this will not work for ADC which we are using""" 
 def SPI_read(spi,bytes):
         data = []   
         try:
